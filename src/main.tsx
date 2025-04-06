b4918b34d9a1936e36903ed3af50f95857741f9d
Hashforgamers <hashforgamers@gmail.com>
2025-04-06 15:59:07 +0000

    fixed bug for speed insight

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Analytics/>
-    <SpeedInsights/>
     <App />
   </StrictMode>
 );
